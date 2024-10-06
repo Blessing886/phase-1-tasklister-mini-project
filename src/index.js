d23dbafbf8c48d979978a251e07332a32796bee2
BlessingOkora <moraablessing082@gmail.com>
2024-10-06 13:30:53 +0300

    Modifies index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".submit");
+const todoList = document.querySelector(".todo-list");
+//event listener
+todoButton.addEventListener("click", addTodo);
+//functions
+function addTodo(event) {
+    event.preventDefault();
+
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //input value
+    todoInput.value = ""
+    // complete button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //append list
+    todoList.appendChild(todoDiv);
+}
+//event listener
+todoList.addEventListener("click", deleteCheck);
+//function
+function deleteCheck(event) {
+    const item = event.target;
+//delete
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        todo.remove();
+    }
+    //done
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("done");
+    }
+}
